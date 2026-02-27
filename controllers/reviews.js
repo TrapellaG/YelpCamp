c533baa8148f0ac8b59f021968382ecc67bdade7
TrapellaG <gerardcampos95@gmail.com>
2026-02-27 12:57:51 +0100

    updated reviews controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,20 +1,20 @@
 const Review = require('../models/review');
-const Campground = require('../models/campground');
+const Spot = require('../models/spot');
 
 module.exports.createReview = async (request, response) => {
-    const campground = await Campground.findById(request.params.id);
+    const spot = await Spot.findById(request.params.id);
     const review = new Review(request.body.review);
     review.author = request.user._id;
-    campground.reviews.push(review);
+    spot.reviews.push(review);
     await review.save();
-    await campground.save();
+    await spot.save();
     request.flash('success', 'Successfully created a new review!');
-    response.redirect(`/spots/${campground._id}`);
+    response.redirect(`/spots/${spot._id}`);
 }
 
 module.exports.deleteReview = async (request, response) => {
     const { id, reviewId } = request.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Spot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
     request.flash('success', 'Successfully deleted review!');
     response.redirect(`/spots/${id}`);
