4780de919b17e18f8e5cfe39831a73cecd5f5406
TrapellaG <gerardcampos95@gmail.com>
2026-02-27 12:32:19 +0100

    mapbox updated

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -24,7 +24,7 @@ mapboxgl.accessToken = mapBoxToken;
             generateId: true,
             // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
             // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-            data: campgrounds,
+            data: spots,
             cluster: true,
             clusterMaxZoom: 14, // Max zoom to cluster points on
             clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
